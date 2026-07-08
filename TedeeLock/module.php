00883a5372d50783e6e719271bb55621c7b07fa8
Florian Graßinger <grassinger@ubyte.gmbh>
2026-07-08 13:29:20 +0200

    Fix: Remove custom RegisterHook to avoid conflict with IPSModuleStrict

--- TedeeLock/module.php
+++ TedeeLock/module.php
@@ -75,28 +75,7 @@ class TedeeLock extends IPSModuleStrict
         }
     }
 
-    protected function RegisterHook(string $WebHook): void
-    {
-        $ids = IPS_GetInstanceListByModuleID("{015A6EB8-D6E5-4B93-B496-0D3F77AE9FE1}");
-        if (sizeof($ids) > 0) {
-            $hooks = json_decode(IPS_GetProperty($ids[0], "Hooks"), true);
-            $found = false;
-            foreach ($hooks as $index => $hook) {
-                if ($hook['Hook'] == $WebHook) {
-                    if ($hook['TargetID'] == $this->InstanceID) {
-                        return;
-                    }
-                    $hooks[$index]['TargetID'] = $this->InstanceID;
-                    $found = true;
-                }
-            }
-            if (!$found) {
-                $hooks[] = ["Hook" => $WebHook, "TargetID" => $this->InstanceID];
-            }
-            IPS_SetProperty($ids[0], "Hooks", json_encode($hooks));
-            IPS_ApplyChanges($ids[0]);
-        }
-    }
+
 
     protected function ProcessHookData()
     {
